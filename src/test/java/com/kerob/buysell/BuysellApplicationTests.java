5438254d1543c8d0edae0f43ead054feb99fe7d1
Qerob <kerob.job@gmail.com>
2022-07-21 15:58:21 +0400

    git init

---
+++ src/test/java/com/kerob/buysell/BuysellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerob.buysell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuysellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
